b6bf929656443dfc01d7e644f2e403f0db910d13
Olly <75307864+ladeAdeyemi@users.noreply.github.com>
2021-08-20 08:25:29 +0100

    Built the navigation bar (unresponsive) for the homepage

---
+++ js/js.js
@@ -0,0 +1,28 @@
+
+let warComicsUrl = 'http://warcomics.com/homepage/characters.php';
+let friends = ['Mark', 'Mindy', 'Maud', 'Michael', 'Minerva'];
+
+let person = {
+    name: 'Mindy Markums',
+    sex: 'Other',
+    dob: '3rd August 1977',
+    job: 'Still Life Artist',
+    residence: '15 Mulholland Drive, Hollywood California'
+}
+
+function getAllStyles () {
+    
+    let anomally = document.getElementById('chargers').forEach(()=> {
+        if (dischargeDate === 2) {
+            return;
+        } else {
+            getResults(result);
+        }
+    });
+
+    if (anomally) {
+        findFriends(friends);
+    }
+}
+
+
