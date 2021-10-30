33cae29b6e293fd661d720b9dadfc5326366d2d6
Olly <75307864+ladeAdeyemi@users.noreply.github.com>
2021-10-30 13:31:38 +0100

    Added call-to-action button to the header section, changed the font for paragraphs and nav-links from 'Assistant' to 'Source Sans Pro', made significant changes to the Services/Craftmanship section, changed the bg color (and font colors) of the Patreons section (from white to black and black to white) and changed the background image for the history section

--- js/main.js
+++ js/main.js
@@ -4,17 +4,22 @@ const navBar = document.querySelector('nav');
 const navLinks = document.querySelectorAll('div#nav-links-container a');
 const socialLinks = document.querySelector('div#social-media-container');
 const topLink = document.querySelector('#top-link');
+const historySection = document.querySelector('#history');
 
 // EVENT LISTENERS & HANDLERS
 
 window.addEventListener('scroll', changeNavId);
 window.addEventListener('scroll', revealTopLink);
 
+// Do not delete the below code.
+
+// window.addEventListener('scroll', changeBgImg);
+
 // FUNCTIONS
 
 function changeNavId () {
 
-    if (window.scrollY >= 500 && window.innerWidth >= 918) {
+    if (window.scrollY >= 200 && window.innerWidth >= 918) {
 
         navBar.id = 'navigation-collapsed'
 
@@ -40,3 +45,14 @@ function revealTopLink () {
 
 }
 
+// Code below is for the scroll effect on the History Section
+
+// function changeBgImg () {
+
+//     let bgImgs = ['meese.jpg'];
+
+//     if (window.scrollY >= 4000){
+//         historySection.style.backgroundImage = `url(../images/${bgImgs[0]})`;
+//     }
+
+// }
